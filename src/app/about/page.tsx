827cc1532cf8baeaacfe1bd29426425538698d8b
dipto230 <diptoroy230305@gmail.com>
2026-01-13 23:35:17 +0530

    working on multiple routes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
 
-export default function AboutPage() {
+import React from 'react'
+export const dynamic = 'force-dynamic'
+
+export default async function AboutPage() {
+
+  //for simulating load time
+await new Promise((resolve)=> setTimeout(resolve,4000))
+
+//for simulating error
+//throw new error("something went wrong")
   return (
     <div>This is AboutPage coming from app</div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout/navbar1";
+import { Navbar } from "@/components/layout/navbar";
+import { ThemeProvider } from "@/providers/ThemProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,13 +25,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         <Navbar/>
-        <h1>This is our root layout</h1>
-        {children}
+        
+          {children}
+          </ThemeProvider>
       </body>
     </html>
   );
